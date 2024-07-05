3891795c9e39882105b6647397d283bed800d9b1
zealzel <zealzel@gmail.com>
2024-07-05 17:06:21 +0800

    updated

--- plugins/disabled.lua
+++ plugins/disabled.lua
@@ -1,3 +1,13 @@
 return {
- -- { "folke/flash.nvim", enabled = false },
+  -- { "folke/flash.nvim", enabled = false },
+  { "nvim-treesitter", enabled = false },
+  { "nvim-treesitter-context", enabled = false },
+  { "nvim-treesitter-textobjects", enabled = false },
+  { "mason-lspconfig", enabled = false },
+  { "mason", enabled = false },
+  { "none-ls", enabled = false },
+  { "flash", enabled = false },
+  { "fugitive", enabled = false },
+  { "neo-tree", enabled = false },
+  { "surround", enabled = false },
 }
