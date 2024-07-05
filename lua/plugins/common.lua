3a3a543a9d8dca21ca740a4dc54676212abf8b63
zealzel <zealzel@gmail.com>
2024-07-05 09:40:53 +0800

    Add plugin markdown-preview

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -53,4 +53,12 @@ return {
       },
     },
   },
+  {
+    -- Markdown preview
+    -- ref: https://www.reddit.com/r/neovim/comments/10w4u51/comment/j7lpl9u/?utm_source=share&utm_medium=web2x&context=3
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
