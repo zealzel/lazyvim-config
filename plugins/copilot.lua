12d1d73e19c7807ed1d05575b6e9410d28d44dca
zealzel <zealzel@gmail.com>
2024-07-08 10:38:19 +0800

    tricks to enable copilot in docker container nvim

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -11,5 +11,9 @@ return {
       help = true,
       -- ["*"] = false,
     },
+    -- copilot_node_command = vim.fn.expand("$HOME") .. "/root/.nvm/versions/node/v22.3.0/bin/node",
+    copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/" .. vim.fn
+      .system("source $HOME/.nvm/nvm.sh && nvm current")
+      :gsub("\n", "") .. "/bin/node",
   },
 }
