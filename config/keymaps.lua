c0d20dbc4ebbc80e46299fe5c97615ee192e630a
zealzel <zealzel@gmail.com>
2024-07-11 14:33:39 +0800

    Add functions moving btw terminal & window. cool. tricks from toggleterm

--- config/keymaps.lua
+++ config/keymaps.lua
@@ -60,7 +60,16 @@ local mappings = {
 local opts = { prefix = "<leader>" }
 wk.register(mappings, opts)
 
--- tmap <C-h> <C-\><C-N><C-h>
--- map("t", "<C-k>", "<C-\\><C-n><C-w>k", { silent = true })
--- map("t", "<C-k>", "<C-\\><C-n><C-w>k", { noremap = true, silent = true })
--- vim.api.nvim_set_keymap("t", "<esc>", "<C-\\><C-n>", { noremap = true })
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
