2e485f821ea6f69aa7c945745bcb558924a916b9
zealzel <zealzel@gmail.com>
2024-07-11 14:31:13 +0800

    Add function : move btw terminal & window. cool. tricks from toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -24,6 +24,7 @@ map("n", "<C-n>", ":Neotree toggle<cr>", { desc = "Neotree toggle", silent = tru
 map("n", "<C-\\>", ":ToggleTerm<cr>", { desc = "toggleterm", silent = true })
 map("n", "<leader>ts", ":ToggleTermSendCurrentLine<cr>", { desc = "ToggleTermSendCurrentLine" })
 map("v", "<leader>ts", ":ToggleTermSendVisualLines<cr>", { desc = ":ToggleTermSendVisualLines<cr>" })
+-- map("v", "<leader>ts", ":ToggleTermSendVisualSelection<cr>", { desc = ":ToggleTermSendVisualSelection<cr>" })
 
 -- whichkeys
 local wk = require("which-key")
@@ -59,3 +60,17 @@ local mappings = {
 }
 local opts = { prefix = "<leader>" }
 wk.register(mappings, opts)
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
