b519fc6c667c823cac1c8a33666532940b9c6e81
zealzel <zealzel@gmail.com>
2024-07-23 10:09:19 +0800

    update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -51,6 +51,7 @@ return {
       lualine_x = {
         {
           "diagnostics",
+          sources = { "nvim_diagnostic", "nvim_lsp" },
           symbols = {
             error = LazyVim.config.icons.diagnostics.Error,
             warn = LazyVim.config.icons.diagnostics.Warn,
@@ -71,50 +72,5 @@ return {
     },
   },
   --   })
-  -- end,
-
-  ------- default as below -------
-  --
-  -- config = function()
-  --   require("lualine").setup({
-  -- options = {
-  --   icons_enabled = true,
-  --   theme = "auto",
-  --   component_separators = { left = "", right = "" },
-  --   section_separators = { left = "", right = "" },
-  --   disabled_filetypes = {
-  --     statusline = {},
-  --     winbar = {},
-  --   },
-  --   ignore_focus = {},
-  --   always_divide_middle = true,
-  --   globalstatus = false,
-  --   refresh = {
-  --     statusline = 1000,
-  --     tabline = 1000,
-  --     winbar = 1000,
-  --   },
-  -- },
-  -- sections = {
-  --   lualine_a = { "mode" },
-  --   lualine_b = { "branch", "diff", "diagnostics" },
-  --   lualine_c = { "filename" },
-  --   lualine_x = { "encoding", "fileformat", "filetype" },
-  --   lualine_y = { "progress" },
-  --   lualine_z = { "location" },
-  -- },
-  -- inactive_sections = {
-  --   lualine_a = {},
-  --   lualine_b = {},
-  --   lualine_c = { "filename" },
-  --   lualine_x = { "location" },
-  --   lualine_y = {},
-  --   lualine_z = {},
-  -- },
-  -- tabline = {},
-  -- winbar = {},
-  -- inactive_winbar = {},
-  -- extensions = {},
-  -- })
   -- end,
 }
