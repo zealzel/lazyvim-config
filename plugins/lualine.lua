dd12ab4299ccac2aff171547bf6f56207b30f4ba
zealzel <zealzel@gmail.com>
2024-07-23 11:30:18 +0800

    update lualine config

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -1,76 +1,73 @@
 return {
   "nvim-lualine/lualine.nvim",
-  -- config = function()
-  --   require("lualine").setup({
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = "auto",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {
-        statusline = {},
-        winbar = {},
-      },
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-      },
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = {
-        { "branch", color = { fg = "yellow" } },
-        LazyVim.lualine.root_dir(),
-        -- { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
-        { LazyVim.lualine.pretty_path() },
-      },
-      lualine_c = {
-        {
-          "diff",
-          symbols = {
-            added = LazyVim.config.icons.git.added,
-            modified = LazyVim.config.icons.git.modified,
-            removed = LazyVim.config.icons.git.removed,
-          },
-          source = function()
-            local gitsigns = vim.b.gitsigns_status_dict
-            if gitsigns then
-              return {
-                added = gitsigns.added,
-                modified = gitsigns.changed,
-                removed = gitsigns.removed,
-              }
-            end
-          end,
-        },
-      },
-      lualine_x = {
-        { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
-        {
-          "diagnostics",
-          sources = { "nvim_diagnostic", "nvim_lsp" },
-          symbols = {
-            error = LazyVim.config.icons.diagnostics.Error,
-            warn = LazyVim.config.icons.diagnostics.Warn,
-            info = LazyVim.config.icons.diagnostics.Info,
-            hint = LazyVim.config.icons.diagnostics.Hint,
+  config = function()
+    require("lualine").setup({
+      opts = {
+        options = {
+          icons_enabled = true,
+          theme = "auto",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          disabled_filetypes = {},
+          ignore_focus = {},
+          always_divide_middle = true,
+          globalstatus = true,
+          refresh = {
+            statusline = 1000,
+            tabline = 1000,
           },
         },
-        {
-          function()
-            return require("noice").api.status.command.get()
-          end,
-          cond = function()
-            return package.loaded["noice"] and require("noice").api.status.command.has()
-          end,
-          color = function()
-            return LazyVim.ui.fg("Statement")
-          end,
-        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = {
+            { "branch", color = { fg = "yellow" } },
+            LazyVim.lualine.root_dir(),
+            -- { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
+            { LazyVim.lualine.pretty_path() },
+          },
+          lualine_c = {
+            {
+              "diff",
+              symbols = {
+                added = LazyVim.config.icons.git.added,
+                modified = LazyVim.config.icons.git.modified,
+                removed = LazyVim.config.icons.git.removed,
+              },
+              source = function()
+                local gitsigns = vim.b.gitsigns_status_dict
+                if gitsigns then
+                  return {
+                    added = gitsigns.added,
+                    modified = gitsigns.changed,
+                    removed = gitsigns.removed,
+                  }
+                end
+              end,
+            },
+          },
+          lualine_x = {
+            { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
+            {
+              "diagnostics",
+              sources = { "nvim_diagnostic", "nvim_lsp" },
+              symbols = {
+                error = LazyVim.config.icons.diagnostics.Error,
+                warn = LazyVim.config.icons.diagnostics.Warn,
+                info = LazyVim.config.icons.diagnostics.Info,
+                hint = LazyVim.config.icons.diagnostics.Hint,
+              },
+            },
+            {
+              function()
+                return require("noice").api.status.command.get()
+              end,
+              cond = function()
+                return package.loaded["noice"] and require("noice").api.status.command.has()
+              end,
+              color = function()
+                return LazyVim.ui.fg("Statement")
+              end,
+            },
         -- stylua: ignore
         {
           function() return require("noice").api.status.mode.get() end,
@@ -89,18 +86,34 @@ return {
           cond = require("lazy.status").has_updates,
           color = function() return LazyVim.ui.fg("Special") end,
         },
+          },
+          lualine_y = {
+            { "progress", separator = " ", padding = { left = 1, right = 0 } },
+            { "location", padding = { left = 0, right = 1 } },
+          },
+          lualine_z = {
+            function()
+              return " " .. os.date("%R")
+            end,
+          },
+        },
       },
-      lualine_y = {
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
-      lualine_z = {
-        function()
-          return " " .. os.date("%R")
-        end,
+      inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
       },
-    },
-  },
-  --   })
-  -- end,
+    })
+  end,
 }
