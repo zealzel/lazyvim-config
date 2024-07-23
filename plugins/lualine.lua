ee4519589964ffff75c4c7a819a825d13d4a140a
zealzel <zealzel@gmail.com>
2024-07-23 10:41:59 +0800

    update lualine config

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -59,6 +59,35 @@ return {
             hint = LazyVim.config.icons.diagnostics.Hint,
           },
         },
+        {
+          function()
+            return require("noice").api.status.command.get()
+          end,
+          cond = function()
+            return package.loaded["noice"] and require("noice").api.status.command.has()
+          end,
+          color = function()
+            return LazyVim.ui.fg("Statement")
+          end,
+        },
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          color = function() return LazyVim.ui.fg("Constant") end,
+        },
+        -- stylua: ignore
+        {
+          function() return "  " .. require("dap").status() end,
+          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+          color = function() return LazyVim.ui.fg("Debug") end,
+        },
+        -- stylua: ignore
+        {
+          require("lazy.status").updates,
+          cond = require("lazy.status").has_updates,
+          color = function() return LazyVim.ui.fg("Special") end,
+        },
       },
       lualine_y = {
         { "progress", separator = " ", padding = { left = 1, right = 0 } },
