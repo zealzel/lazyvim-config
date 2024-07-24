9ac9ef5cef4cb9830161d6fd664e4c50b2e85e67
zealzel <zealzel@gmail.com>
2024-07-24 21:12:19 +0800

    update

--- plugins/disabled.lua
+++ plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   -- { "folke/flash.nvim", enabled = false },
   -- to avoid No C compiler found issue.
   -- ref:https://github.com/LazyVim/LazyVim/discussions/1920
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  -- { "williamboman/mason-lspconfig.nvim", enabled = false },
 }
