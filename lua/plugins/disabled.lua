ee9c15d4f2284980ae83e82e1536767a10a1dfbc
zealzel <zealzel@gmail.com>
2024-07-24 21:12:42 +0800

    update

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,5 +3,5 @@ return {
   --
   -- to avoid No C compiler found issue.
   -- ref:https://github.com/LazyVim/LazyVim/discussions/1920
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  -- { "williamboman/mason-lspconfig.nvim", enabled = false },
 }
