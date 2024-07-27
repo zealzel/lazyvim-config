530e7d9bf9533031b65901ebcf4bc57f5f075bd3
zealzel <zealzel@gmail.com>
2024-07-27 06:47:42 +0000

    Use none-ls to set cmake_format detailed config

--- plugins/conform.lua
+++ plugins/conform.lua
@@ -25,11 +25,34 @@ return {
     for _, ft in ipairs(supported) do
       opts.formatters_by_ft[ft] = { "prettier" }
     end
+    -- opts.formatters_by_ft = {
+    --   cmake = { "cmake_format" },
+    -- }
+
     opts.formatters = opts.formatters or {}
     opts.formatters.prettier = {
       -- condition = function(_, ctx)
       --   return M.has_parser(ctx) and (vim.g.lazyvim_prettier_needs_config ~= true or M.has_config(ctx))
       -- end,
     }
+    -- cmake_format detailed config does not work using conform.nvim. use none-ls instead.
+    -- below are failed trails
+    --
+    -- opts.formatters = {
+    --   cmake_format = {
+    --     -- condition = function(_, ctx)
+    --     --   return vim.fs.find({ "cmakeformat.yml" }, { path = ctx.filename, upward = true })[1]
+    --     -- end,
+    --     command = "cmake-format",
+    --     args = { "--tab-size", 4 },
+    --   },
+    -- }
+    -- opts.formatters.cmake_format = {
+    --   -- condition = function(_, ctx)
+    --   --   return vim.fs.find({ "cmakeformat.yml" }, { path = ctx.filename, upward = true })[1]
+    --   -- end,
+    --   command = "cmake-format",
+    --   args = "--tab-size=4",
+    -- }
   end,
 }
--- plugins/none-ls.lua
+++ plugins/none-ls.lua
@@ -22,6 +22,13 @@ return {
     opts.sources = opts.sources or {}
     -- table.insert(opts.sources, nls.builtins.formatting.prettier) -- prettier extra plugin
     -- table.insert(opts.sources, nls.builtins.formatting.black) -- black extra plugin
-    table.insert(opts.sources, nls.builtins.formatting.cmake_format)
+    -- table.insert(opts.sources, nls.builtins.formatting.cmake_format)
+
+    opts.sources = vim.list_extend(opts.sources, {
+      -- nls.builtins.formatting.prettier,
+      nls.builtins.formatting.cmake_format.with({
+        extra_args = { "--line-width", "100", "--tab-size", "2", "--dangle-parens", "true", "--min-prefix-chars", "12" },
+      }),
+    })
   end,
 }
