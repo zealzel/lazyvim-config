b4069bfa5a17ed241f47a95c2c25cbadf54c6035
zealzel <zealzel@gmail.com>
2024-07-28 07:26:09 +0000

    Add python.lua.bak

--- plugins/none-ls.lua
+++ plugins/none-ls.lua
@@ -1,29 +1,12 @@
 return {
-  -- "nvimtools/none-ls.nvim",
-  -- optional = true,
-  -- config = function()
-  --   local nls = require("null-ls")
-  --   nls.setup({
-  --     sources = {
-  --       nls.builtins.formatting.black,
-  --       nls.builtins.formatting.stylua,
-  --       nls.builtins.formatting.shfmt,
-  --       -- nls.builtins.diagnostics.mypy,
-  --       -- nls.builtins.completion.spell,
-  --     },
-  --   })
-  -- end,
-  -- opts = { border = "rounded" },
-  --
   "nvimtools/none-ls.nvim",
-  optional = true,
+  -- optional = true,
   opts = function(_, opts)
     local nls = require("null-ls")
     opts.sources = opts.sources or {}
     -- table.insert(opts.sources, nls.builtins.formatting.prettier) -- prettier extra plugin
     -- table.insert(opts.sources, nls.builtins.formatting.black) -- black extra plugin
     -- table.insert(opts.sources, nls.builtins.formatting.cmake_format)
-
     opts.sources = vim.list_extend(opts.sources, {
       -- nls.builtins.formatting.prettier,
       nls.builtins.formatting.cmake_format.with({
