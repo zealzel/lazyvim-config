e058462a7b9a6e40e0db2e505cb0b48b320bf055
zealzel <zealzel@gmail.com>
2024-07-28 08:40:10 +0000

    update python.lua to enable ruff

--- plugins/python.lua
+++ plugins/python.lua
@@ -12,6 +12,38 @@ return {
           },
         },
       }
+      opts.servers.ruff = {
+        cmd_env = { RUFF_TRACE = "messages" },
+        init_options = {
+          settings = {
+            logLevel = "error",
+          },
+        },
+        keys = {
+          {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+        },
+      }
+      -- opts.servers.ruff_lsp = {
+      --   keys = {
+      --     {
+      --       "<leader>co",
+      --       LazyVim.lsp.action["source.organizeImports"],
+      --       desc = "Organize Imports",
+      --     },
+      --   },
+      -- }
+      -- opts.setup = {
+      --   [ruff] = function()
+      --     LazyVim.lsp.on_attach(function(client, _)
+      --       -- Disable hover in favor of Pyright
+      --       client.server_capabilities.hoverProvider = false
+      --     end, ruff)
+      --   end,
+      -- }
     end,
   },
   -- Uncomment below if you want to use black besides ruff
