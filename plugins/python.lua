c4c67e7db3207e32e915b1f6b1da9ca2521a4c26
zealzel <zealzel@gmail.com>
2024-07-29 01:11:46 +0800

    sync with mac & min

--- plugins/python.lua
+++ plugins/python.lua
@@ -5,35 +5,37 @@ return {
       opts.servers.basedpyright = {
         settings = {
           basedpyright = {
-            analysis = { typeCheckingMode = "off" },
+            analysis = { typeCheckingMode = "off" }, -- strict|standard|basic|off
           },
         },
       }
     end,
   },
-  {
-    "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft["python"] = { "black" }
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    enabled = true,
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "black")
-      -- table.insert(opts.ensure_installed, "flake8")
-      -- table.insert(opts.ensure_installed, "mypy")
-      opts.ui.border = "rounded"
-    end,
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    -- optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, nls.builtins.formatting.black)
-    end,
-  },
+  -- Uncomment below if you want to use black besides ruff
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = function(_, opts)
+  --     opts.formatters_by_ft["python"] = { "black" }
+  --   end,
+  -- },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   enabled = true,
+  --   opts = function(_, opts)
+  --     table.insert(opts.ensure_installed, "black")
+  --     -- table.insert(opts.ensure_installed, "flake8")
+  --     -- table.insert(opts.ensure_installed, "mypy")
+  --     opts.ui.border = "rounded"
+  --   end,
+  -- },
+  -- Uncomment below if you want to use black provided by none-ls
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   -- optional = true,
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = opts.sources or {}
+  --     table.insert(opts.sources, nls.builtins.formatting.black)
+  --   end,
+  -- },
 }
