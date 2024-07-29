cfd4615b33eafe54b73cee7ea44fc96b1f83d579
zealzel <zealzel@gmail.com>
2024-07-29 21:59:49 +0800

    delete no-used

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -6,17 +6,5 @@ return {
     opts.diagnostics = {
       float = { border = "rounded" },
     }
-    -- python lsp: basedpyright
-    -- opts.servers.basedpyright = {
-    --   settings = {
-    --     basedpyright = {
-    --       analysis = { typeCheckingMode = "off" },
-    --     },
-    --   },
-    -- }
-    -- c/c++ lsp: clangd
-    -- opts.setup.clangd = function(_, opts)
-    --   opts.capabilities.offsetEncoding = { "utf-16" }
-    -- end
   end,
 }
