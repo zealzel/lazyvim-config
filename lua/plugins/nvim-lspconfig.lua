47db6bacffcde7a0b0a33a568c1e6a6e89d2a5ae
zealzel <zealzel@gmail.com>
2024-07-29 14:08:13 +0800

    Change LspInfo windo to rounded

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
+    -- opts.ui.windows.border = "single"
+    require("lspconfig.ui.windows").default_options.border = "rounded"
     opts.diagnostics = {
       float = { border = "rounded" },
     }
