4d56bda6aa090f0632a95a25f1ce06f88533d041
zealzel <zealzel@gmail.com>
2024-07-29 21:37:16 +0800

    Add more example files

---
+++ examples/some.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+
+
+# demo class
+class Someclass:
+    def __init__(self):
+        print("Someclass init")
+
+    def demo(self):
+        print("Someclass demo")
+        return 0
+
+
+# demo function
+def somefunc():
+    print("somefunc")
+    return 0
+
+
+if __name__ == "__main__":
+    print("SDF")
