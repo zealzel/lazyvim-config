fd01da100c67f56b1da59d48a81d83f93c662fa3
zealzel <zealzel@gmail.com>
2024-07-31 18:32:35 +0800

    copy Lazyvim's markdown extra into plugins/ as mine own and diable the usage of extra

---
+++ plugins/markdown.lazyvim.lua
@@ -0,0 +1,215 @@
+local color1_bg = "#f265b5"
+local color2_bg = "#37f499"
+local color3_bg = "#04d1f9"
+local color4_bg = "#a48cf2"
+local color5_bg = "#f1fc79"
+local color6_bg = "#f7c67f"
+local color_fg = "#323449"
+
+LazyVim.on_very_lazy(function()
+  vim.filetype.add({
+    extension = { mdx = "markdown.mdx" },
+  })
+end)
+return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = { "markdown", "markdown.mdx" },
+      root = "README.md",
+    })
+  end,
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters = {
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
+              end
+            end
+          end,
+        },
+        ["markdownlint-cli2"] = {
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
+        },
+      },
+      formatters_by_ft = {
+        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "markdownlint-cli2", "markdown-toc" } },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources or {}, {
+        nls.builtins.diagnostics.markdownlint_cli2,
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        markdown = { "markdownlint-cli2" },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {},
+      },
+    },
+  },
+
+  -- Markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    -- build = function()
+    --   vim.fn["mkdp#util#install"]()
+    -- end,
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    opts = {
+      file_types = { "markdown", "norg", "rmd", "org" },
+      code = {
+        sign = false,
+        right_pad = 1,
+      },
+      anti_conceal = {
+        -- This enables hiding any added text on the line the cursor is on
+        -- This does have a performance penalty as we must listen to the 'CursorMoved' event
+        enabled = false,
+      },
+      heading = {
+        sign = false,
+        position = "overlay", -- inline | overlay
+        width = "full", -- | block | full
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+
+        -- Heading colors (when not hovered over), extends through the entire line
+        vim.cmd(string.format([[highlight Headline1Bg guifg=%s guibg=%s]], color_fg, color1_bg)),
+        vim.cmd(string.format([[highlight Headline2Bg guifg=%s guibg=%s]], color_fg, color2_bg)),
+        vim.cmd(string.format([[highlight Headline3Bg guifg=%s guibg=%s]], color_fg, color3_bg)),
+        vim.cmd(string.format([[highlight Headline4Bg guifg=%s guibg=%s]], color_fg, color4_bg)),
+        vim.cmd(string.format([[highlight Headline5Bg guifg=%s guibg=%s]], color_fg, color5_bg)),
+        vim.cmd(string.format([[highlight Headline6Bg guifg=%s guibg=%s]], color_fg, color6_bg)),
+
+        -- Highlight for the heading and sign icons (symbol on the left)
+        -- I have the sign disabled for now, so this makes no effect
+        vim.cmd(string.format([[highlight Headline1Fg cterm=bold gui=bold guifg=%s]], color1_bg)),
+        vim.cmd(string.format([[highlight Headline2Fg cterm=bold gui=bold guifg=%s]], color2_bg)),
+        vim.cmd(string.format([[highlight Headline3Fg cterm=bold gui=bold guifg=%s]], color3_bg)),
+        vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg)),
+        vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg)),
+        vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg)),
+
+        backgrounds = {
+          "Headline1Bg",
+          "Headline2Bg",
+          "Headline3Bg",
+          "Headline4Bg",
+          "Headline5Bg",
+          "Headline6Bg",
+        },
+        foregrounds = {
+          "Headline1Fg",
+          "Headline2Fg",
+          "Headline3Fg",
+          "Headline4Fg",
+          "Headline5Fg",
+          "Headline6Fg",
+        },
+      },
+    },
+    code = {
+      -- Turn on / off code block & inline code rendering
+      enabled = true,
+      -- Turn on / off any sign column related rendering
+      sign = true,
+      -- Determines how code blocks & inline code are rendered:
+      --  none: disables all rendering
+      --  normal: adds highlight group to code blocks & inline code, adds padding to code blocks
+      --  language: adds language icon to sign column if enabled and icon + name above code blocks
+      --  full: normal + language
+      style = "full",
+      -- Amount of padding to add to the left of code blocks
+      left_pad = 0,
+      -- Amount of padding to add to the right of code blocks when width is 'block'
+      right_pad = 0,
+      -- Width of the code block background:
+      --  block: width of the code block
+      --  full: full width of the window
+      width = "full",
+      -- Determins how the top / bottom of code block are rendered:
+      --  thick: use the same highlight as the code body
+      --  thin: when lines are empty overlay the above & below icons
+      border = "thick",
+      -- Used above code blocks for thin border
+      -- above = "▄",
+      -- below = "▀",
+      -- above = "▄",
+      -- below = "▄",
+      -- Used below code blocks for thin border
+      above = "▀",
+      below = "▀",
+      -- Highlight for code blocks & inline code
+      highlight = "RenderMarkdownCode",
+      highlight_inline = "RenderMarkdownCodeInline",
+    },
+    -- ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      LazyVim.toggle.map("<leader>um", {
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      })
+    end,
+  },
+}
