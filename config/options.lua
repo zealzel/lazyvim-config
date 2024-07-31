b88892590b76bbab4360d08f2240a1a5f3a14f55
zealzel <zealzel@gmail.com>
2024-08-01 02:00:43 +0800

    fix folding open markder not shown correctly issue

--- config/options.lua
+++ config/options.lua
@@ -11,14 +11,24 @@ opt.scrolloff = 0
 opt.modeline = false
 opt.timeoutlen = 1000
 
+opt.swapfile = false
+
 -- vim.g.mkdp_auto_start = 0
 -- vim.g.mkdp_auto_close = 0
 -- vim.g.mkdp_browser = "firefox"
+-- vim.g.maplocalleader = ","
 
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 99
+vim.g.lazyvim_statuscolumn = {
+  folds_open = true, -- show fold sign when fold is open, default: false
+  folds_githl = false, -- highlight fold sign with git sign color, default: false
+}
 
 vim.o.shell = "bash"
 
+-- set python lsp as basedpyright instead of pyright
+-- https://github.com/DetachHead/basedpyright
+-- ref: https://github.com/LazyVim/LazyVim/discussions/3350
 vim.g.lazyvim_python_lsp = "basedpyright"
