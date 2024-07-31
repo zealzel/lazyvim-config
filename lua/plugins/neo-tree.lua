bd979066011c3343bcaeceea5acb17b56982f113
zealzel <zealzel@gmail.com>
2024-07-31 23:55:07 +0800

    fix original relative-path not working bug

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,35 @@
+local log = require("plenary.log").new({
+  plugin = "my-plugin",
+  level = "debug",
+})
+
+local function get_relative_path(base, target)
+  local base_parts = vim.split(base, "/")
+  local target_parts = vim.split(target, "/")
+
+  while #base_parts > 0 and #target_parts > 0 and base_parts[1] == target_parts[1] do
+    table.remove(base_parts, 1)
+    table.remove(target_parts, 1)
+  end
+
+  return string.rep("../", #base_parts) .. table.concat(target_parts, "/")
+end
+
 local function copy_path(state)
   -- ref: https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370
   -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
   -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+  local current_work_dir = vim.fn.getcwd()
   local node = state.tree:get_node()
   local filepath = node:get_id()
   local filename = node.name
   local modify = vim.fn.fnamemodify
-
+  local relative_path = get_relative_path(current_work_dir, filepath)
+  -- log.debug("relative_path: " .. relative_path)
   local results = {
     filepath,
-    modify(filepath, ":."),
+    get_relative_path(current_work_dir, filepath),
+    -- modify(filepath, ":."),
     modify(filepath, ":~"),
     filename,
     modify(filename, ":r"),
