a0062464be65d9ef8cbff2e89afcd2e2e35b15c0
zealzel <zealzel@gmail.com>
2024-07-31 16:39:51 +0800

    Use Neogit to replace fugitive

--- plugins/common.lua
+++ plugins/common.lua
@@ -1,4 +1,16 @@
 return {
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+  },
   { "ellisonleao/gruvbox.nvim" },
   {
     -- Configure LazyVim to load gruvbox
