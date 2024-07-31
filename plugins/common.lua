6bf1411e0c3414c78a9904803292c19668251fb3
zealzel <zealzel@gmail.com>
2024-07-31 16:35:24 +0800

    Add new plugin Neogit to replace fugitive

--- plugins/common.lua
+++ plugins/common.lua
@@ -1,5 +1,17 @@
 return {
-  "tpope/vim-fugitive",
+  -- "tpope/vim-fugitive",
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = true,
+  },
   "williamboman/mason-lspconfig.nvim",
   "ellisonleao/gruvbox.nvim",
   "sunaku/tmux-navigate",
