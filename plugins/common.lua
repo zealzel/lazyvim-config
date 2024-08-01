82a7068d69837ec10e98f089d248accbc95d47e2
zealzel <zealzel@gmail.com>
2024-08-01 13:41:28 +0800

    delete no-used codes in common.lua

--- plugins/common.lua
+++ plugins/common.lua
@@ -57,11 +57,6 @@ return {
   {
     "echasnovski/mini.nvim",
     version = false,
-    -- config = function()
-    --   require("mini.animate").setup({
-    --     -- Configuration here, or leave empty to use defaults
-    --   })
-    -- end,
   },
   {
     "folke/edgy.nvim",
