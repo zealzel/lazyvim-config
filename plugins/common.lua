06d955bf89a9521a5d820ccf33251beed1e2ddab
zealzel <zealzel@gmail.com>
2024-08-01 13:41:51 +0800

    delete no-used codes in common.lua

--- plugins/common.lua
+++ plugins/common.lua
@@ -55,33 +55,9 @@ return {
       },
     },
   },
-  -- {
-  --   -- Markdown preview
-  --   -- ref: https://www.reddit.com/r/neovim/comments/10w4u51/comment/j7lpl9u/?utm_source=share&utm_medium=web2x&context=3
-  --   "iamcco/markdown-preview.nvim",
-  --   build = function()
-  --     vim.fn["mkdp#util#install"]()
-  --   end,
-  -- },
-  -- {
-  --   "ellisonleao/glow.nvim",
-  --   config = true,
-  --   cmd = "Glow",
-  --   opts = {
-  --     -- width = 80,
-  --     -- height = 100,
-  --     width = 160,
-  --     height = 200,
-  --   },
-  -- },
   {
     "echasnovski/mini.nvim",
     version = false,
-    -- config = function()
-    --   require("mini.animate").setup({
-    --     -- Configuration here, or leave empty to use defaults
-    --   })
-    -- end,
   },
   {
     "folke/edgy.nvim",
