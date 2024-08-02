eeebb2b911944ddac4ef81aada2aa114ac5a18f4
zealzel <zealzel@gmail.com>
2024-08-02 22:44:43 +0800

    1. use transparent.nvim to replace transparent.nvim 2. add new plugin nvim-highlight-colors for color highlight 3. add new plugin incline.nvim to replace lualine winbar setting

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -114,6 +114,40 @@ return {
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
   "sindrets/diffview.nvim",
-  "tribela/vim-transparent",
+  -- "tribela/vim-transparent",
+  {
+    -- https://github.com/xiyaowong/transparent.nvim
+    "xiyaowong/transparent.nvim",
+    opts = {
+      -- exclude_groups = { "CursorLine" },
+    },
+  },
   { "brenoprata10/nvim-highlight-colors", config = true },
+  {
+    "b0o/incline.nvim",
+    config = true,
+    opts = {
+      window = {
+        padding = 0,
+        margin = { horizontal = 0 },
+      },
+      render = function(props)
+        local helpers = require("incline.helpers")
+        local devicons = require("nvim-web-devicons")
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        if filename == "" then
+          filename = "[No Name]"
+        end
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+        return {
+          ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+          " ",
+          { filename, gui = modified and "bold,italic" or "bold" },
+          " ",
+          guibg = "#44406e",
+        }
+      end,
+    },
+  },
 }
