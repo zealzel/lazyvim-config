8577f9ee18cd6e9f34250bcdec4acadf1ab9207b
zealzel <zealzel@gmail.com>
2024-08-08 01:25:58 +0800

    fix copy wrong lua. indent-blankline.lua.

--- plugins/indent-blankline.lua
+++ plugins/indent-blankline.lua
@@ -1,140 +1,43 @@
-local function get_venv(variable)
-  local venv = os.getenv(variable)
-  if venv ~= nil and string.find(venv, "/") then
-    local orig_venv = venv
-    for w in orig_venv:gmatch("([^/]+)") do
-      venv = w
-    end
-    venv = string.format("%s", venv)
-  end
-  return venv
-end
-
 return {
-  "nvim-lualine/lualine.nvim",
-  -- config = function()
-  --   require("lualine").setup({
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = "auto",
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      disabled_filetypes = {},
-      ignore_focus = {},
-      always_divide_middle = true,
-      globalstatus = true,
-      refresh = {
-        statusline = 1000,
-        tabline = 1000,
-      },
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = {
-        { "branch", color = { fg = "yellow" } },
-        LazyVim.lualine.root_dir(),
-        -- { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
-        { LazyVim.lualine.pretty_path() },
-      },
-      lualine_c = {
-        {
-          "diff",
-          symbols = {
-            added = LazyVim.config.icons.git.added,
-            modified = LazyVim.config.icons.git.modified,
-            removed = LazyVim.config.icons.git.removed,
-          },
-          source = function()
-            local gitsigns = vim.b.gitsigns_status_dict
-            if gitsigns then
-              return {
-                added = gitsigns.added,
-                modified = gitsigns.changed,
-                removed = gitsigns.removed,
-              }
-            end
-          end,
-        },
-      },
-      lualine_x = {
-        {
-          function()
-            local venv = get_venv("CONDA_DEFAULT_ENV") or get_venv("VIRTUAL_ENV") or "NO ENV"
-            return " " .. venv
-          end,
-          cond = function()
-            return vim.bo.filetype == "python"
-          end,
-        },
-        { "filetype", icon_only = false, separator = "", padding = { left = 1, right = 0 } },
-        {
-          "diagnostics",
-          sources = { "nvim_diagnostic", "nvim_lsp" },
-          symbols = {
-            error = LazyVim.config.icons.diagnostics.Error,
-            warn = LazyVim.config.icons.diagnostics.Warn,
-            info = LazyVim.config.icons.diagnostics.Info,
-            hint = LazyVim.config.icons.diagnostics.Hint,
-          },
-        },
-        {
-          function()
-            return require("noice").api.status.command.get()
-          end,
-          cond = function()
-            return package.loaded["noice"] and require("noice").api.status.command.has()
-          end,
-          color = function()
-            return LazyVim.ui.fg("Statement")
-          end,
-        },
-        -- stylua: ignore
-        {
-          function() return require("noice").api.status.mode.get() end,
-          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-          color = function() return LazyVim.ui.fg("Constant") end,
-        },
-        -- stylua: ignore
-        {
-          function() return "  " .. require("dap").status() end,
-          cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
-          color = function() return LazyVim.ui.fg("Debug") end,
-        },
-        -- stylua: ignore
-        {
-          require("lazy.status").updates,
-          cond = require("lazy.status").has_updates,
-          color = function() return LazyVim.ui.fg("Special") end,
-        },
-      },
-      lualine_y = {
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
-      },
-      lualine_z = {
-        function()
-          return " " .. os.date("%R")
-        end,
-      },
-    },
-    -- winbar = {
-    --   lualine_a = {},
-    --   lualine_b = {},
-    --   lualine_c = { "filename" },
-    --   lualine_x = {},
-    --   lualine_y = {},
-    --   lualine_z = {},
-    -- },
-    -- inactive_winbar = {
-    --   lualine_a = {},
-    --   lualine_b = {},
-    --   lualine_c = { "filename" },
-    --   lualine_x = {},
-    --   lualine_y = {},
-    --   lualine_z = {},
-    -- },
-  },
-  --   })
-  -- end,
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = function(_, opts)
+    local highlight = {
+      "RainbowYellow",
+      "RainbowCyan",
+      "RainbowBlue",
+      "RainbowRed",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+    }
+    local hooks = require("ibl.hooks")
+
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+    -- opts.indent = {
+    --   highlight = highlight,
+    --   -- char = "",
+    -- }
+    -- opts.whitespace = {
+    --   highlight = highlight,
+    --   remove_blankline_trail = false,
+    -- }
+    -- opts.scope = { enabled = true }
+    -- opts.indent = { char = "┊" }
+    opts.scope = {
+      enabled = true,
+      highlight = highlight,
+      -- char = "┊",
+    }
+  end,
 }
