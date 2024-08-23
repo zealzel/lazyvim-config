f8b1eb460602dfaf9b631fbc8d55ed477442beb5
zealzel <zealzel@gmail.com>
2024-08-23 10:41:51 +0800

    chnage colorschem of markdown heading

--- plugins/markdown.lazyvim.lua
+++ plugins/markdown.lazyvim.lua
@@ -1,16 +1,29 @@
-local color1_bg = "#f265b5"
-local color2_bg = "#37f499"
-local color3_bg = "#04d1f9"
-local color4_bg = "#a48cf2"
-local color5_bg = "#f1fc79"
-local color6_bg = "#f7c67f"
-local color_fg = "#323449"
+-- local color1_bg = "#f265b5"
+-- local color2_bg = "#37f499"
+-- local color3_bg = "#04d1f9"
+-- local color4_bg = "#a48cf2"
+-- local color5_bg = "#f1fc79"
+-- local color6_bg = "#f7c67f"
+-- local color_fg = "#323449"
+
+local color1_bg = "#2e3440" -- 深藍灰色，穩定且適合 H1
+local color2_bg = "#3b4252" -- 深灰藍色，較淺但依然有份量
+local color3_bg = "#4c566a" -- 中性灰藍色，易於閱讀
+local color4_bg = "#5e81ac" -- 柔和的藍色，提升辨識度
+local color5_bg = "#81a1c1" -- 淺藍灰色，增加層次感
+local color6_bg = "#a3be8c" -- 柔和的綠色，為最低層級提供對比
+local color5_bg = "#7b8794" -- 比之前略深的灰藍色，保證清晰度
+local color6_bg = "#9aa5b1" -- 明顯的灰藍色，讓H6更清晰
+local color6_bg = "#b48ead" -- 柔和的紫灰色，與其他色調形成區別
+local color_fg = "#e5e9f0" -- 柔和的淺灰白色，保持文字清晰易讀
+local color_fg = "#c0c5ce" -- 更暗的中性灰色，減少眼睛疲勞
 
 LazyVim.on_very_lazy(function()
   vim.filetype.add({
     extension = { mdx = "markdown.mdx" },
   })
 end)
+
 return {
   recommended = function()
     return LazyVim.extras.wants({
@@ -42,8 +55,10 @@ return {
         },
       },
       formatters_by_ft = {
-        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
-        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        -- ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        -- ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
       },
     },
   },
@@ -63,10 +78,12 @@ return {
   },
   {
     "mfussenegger/nvim-lint",
-    optional = true,
     opts = {
       linters_by_ft = {
         markdown = { "markdownlint-cli2" },
+        -- markdown = { "markdownlint-cli2", "vale" },
+        -- markdown = { "markdownlint-cli2", "markdown-toc" },
+        -- markdown = { "vale", "write-good" },
       },
     },
   },
@@ -78,11 +95,9 @@ return {
       },
     },
   },
-
-  -- Markdown preview
-  {
+  { -- Markdown preview
     "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    -- cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     -- build = function()
     --   vim.fn["mkdp#util#install"]()
@@ -90,6 +105,8 @@ return {
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_markdown_css = "/Users/zealzel/.dotfiles/lazyvim-config/markdown_iamcco.css"
+      -- vim.g.mkdp_markdown_css = "~/.dotfiles/lazyvim-config/markdown_iamcco.css"
     end,
     keys = {
       {
@@ -101,13 +118,34 @@ return {
     },
     config = function()
       vim.cmd([[do FileType]])
+      vim.cmd([[
+        function OpenMarkdownPreview (url)
+            execute "silent ! open -a 'Google Chrome' -n --args --new-window " . a:url
+        endfunction
+      ]])
+      vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
     end,
   },
+  {
+    "OXY2DEV/markview.nvim",
+    enabled = false,
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
 
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
   {
     "MeanderingProgrammer/markdown.nvim",
+    enabled = true,
     opts = {
-      file_types = { "markdown", "norg", "rmd", "org" },
+      file_types = { "markdown", "norg", "rmd", "org", "Avante" },
       code = {
         sign = false,
         right_pad = 1,
@@ -139,6 +177,8 @@ return {
         vim.cmd(string.format([[highlight Headline4Fg cterm=bold gui=bold guifg=%s]], color4_bg)),
         vim.cmd(string.format([[highlight Headline5Fg cterm=bold gui=bold guifg=%s]], color5_bg)),
         vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg)),
+        vim.api.nvim_set_hl(0, "RenderMarkdownCode", { fg = "NONE", bg = "#202020" }),
+        -- vim.api.nvim_set_hl(0, "@neorg.tags.ranged_verbatim.code_block", { fg = "NONE", bg = "#202020" }),
 
         backgrounds = {
           "Headline1Bg",
@@ -182,18 +222,22 @@ return {
       --  thin: when lines are empty overlay the above & below icons
       border = "thick",
       -- Used above code blocks for thin border
+      above = "▀",
+      below = "▄",
       -- above = "▄",
       -- below = "▀",
+      --
       -- above = "▄",
       -- below = "▄",
       -- Used below code blocks for thin border
-      above = "▀",
-      below = "▀",
+      -- above = "▀",
+      -- below = "▀",
       -- Highlight for code blocks & inline code
       highlight = "RenderMarkdownCode",
       highlight_inline = "RenderMarkdownCodeInline",
     },
     -- ft = { "markdown", "norg", "rmd", "org" },
+    ft = { "markdown", "Avante", "norg" },
     config = function(_, opts)
       require("render-markdown").setup(opts)
       LazyVim.toggle.map("<leader>um", {
