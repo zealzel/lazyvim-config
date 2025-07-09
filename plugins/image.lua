e4cb2814bf367d66012fba13d89b2f159e26a7e5
tw13814 <zealzel@tw13814.local>
2025-07-09 23:26:19 +0800

    updated

--- plugins/image.lua
+++ plugins/image.lua
@@ -1,8 +1,10 @@
 return {
   "3rd/image.nvim",
+  build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
   enabled = true,
   opts = {
     backend = "kitty",
+    processor = "magick_cli",
     integrations = {
       markdown = {
         enabled = true,
@@ -12,14 +14,14 @@ return {
         only_render_image_at_cursor = true,
         filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
       },
-      neorg = {
-        enabled = true,
-        clear_in_insert_mode = false,
-        download_remote_images = true,
-        -- only_render_image_at_cursor = false,
-        only_render_image_at_cursor = true,
-        filetypes = { "norg" },
-      },
+      -- neorg = {
+      --   enabled = true,
+      --   clear_in_insert_mode = false,
+      --   download_remote_images = true,
+      --   -- only_render_image_at_cursor = false,
+      --   only_render_image_at_cursor = true,
+      --   filetypes = { "norg" },
+      -- },
       html = {
         enabled = false,
       },
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,5 +1,4 @@
 return {
-  -- "williamboman/mason.nvim",
   "mason-org/mason.nvim",
   opts = {
     ui = {
